044626daf2acceef080b37931100e3c549929ecb
Stephan Bergemann <oberling@deroberling.de>
2013-04-15 22:31:21 +0200

    fixed some internationalization issues with the bootstrap theme

--- htdocs/themes/bootstrap/views/about.php
+++ htdocs/themes/bootstrap/views/about.php
@@ -1,8 +1,15 @@
 <?php $this->load->view("defaults/header");?>
 
-<div class="about">
-	<h1>About</h1>
-	To edit this page edit application/views/about.php.
+<div class="row">
+	<div class="span12">
+		<div class="page-header">
+<?php /* <div class="about"> */ ?>
+			<h1>About</h1>
+		</div>
+	</div>
+	<div class="span12">
+		To edit this page edit application/views/about.php.
+	</div>
 </div>
 
 <?php $this->load->view("defaults/footer");?>
