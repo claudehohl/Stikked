7d76e03c40dd926ba26c731dea6cc538e2aa2bd8
esplor <esplor@gmail.com>
2013-08-13 19:53:36 +0200

    Update form_validation_lang.php

--- htdocs/application/language/norwegian/form_validation_lang.php
+++ htdocs/application/language/norwegian/form_validation_lang.php
@@ -1,23 +1,23 @@
 <?php
 
 $lang['required']		= "Feltet %s er obligatorisk.";
-$lang['not_allowed']		= "You are not allowed to paste.";
-$lang['captcha']	    	= "The captcha is incorrect.";
+$lang['not_allowed']		= "Du er ikke tillatt til å poste.";
+$lang['captcha']	    	= "Captcha er feil.";
 $lang['blocked_words']    	= "Din post inneholder blokkerte ord.";
 $lang['robot']             	= "Robot, stikk av!";
 $lang['empty']			= "Feltet %s kan ikke være tomt.";
-$lang['valid_lang']		= "Please select your language.";
+$lang['valid_lang']		= "Vennligst velg ditt språk.";
 $lang['isset']			= "Feltet %s må ha en verdi.";
-$lang['valid_email']		= "Feltet %s field must contain a valid email address.";
-$lang['valid_emails']		= "Feltet %s field must contain all valid email addresses.";
-$lang['valid_url']		= "Feltet %s field must contain a valid URL.";
-$lang['valid_ip']		= "Feltet %s field must contain a valid IP.";
-$lang['min_length']		= "Feltet %s field must be at least %s characters in length.";
-$lang['max_length']		= "Feltet %s field can not exceed %s characters in length.";
-$lang['exact_length']		= "Feltet %s field must be exactly %s characters in length.";
-$lang['alpha']			= "Feltet %s field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "Feltet %s field may only contain alpha-numeric characters.";
-$lang['alpha_dash']		= "Feltet %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['valid_email']		= "Feltet %s må inneholde en gyldig epost adresse.";
+$lang['valid_emails']		= "Feltet %s må inneholde gyldige epost adresser.";
+$lang['valid_url']		= "Feltet %s må inneholde en gyldig URL.";
+$lang['valid_ip']		= "Feltet %s må inneholde en gyldig IP.";
+$lang['min_length']		= "Feltet %s må inneholde minst %s tegn.";
+$lang['max_length']		= "Feltet %s kan ikke være lengre enn %s tegn.";
+$lang['exact_length']		= "Feltet %s må være akkurat %s tegn.";
+$lang['alpha']			= "Feltet %s kan bare inneholde alfabetiske tegn.";
+$lang['alpha_numeric']		= "Feltet %s kan bare inneholde alfanumeriske tegn.";
+$lang['alpha_dash']		= "Feltet %s kan bare inneholde alfanumeriske tegn, understrek og bindestrek.";
 $lang['numeric']		= "Feltet %s field must contain only numbers.";
 $lang['is_numeric']		= "Feltet %s field must contain only numeric characters.";
 $lang['integer']		= "Feltet %s field must contain an integer.";
