37572b0e01b8254ea7926f2e0cae8f0138c3f42f
Xiaoxing <ye@xiaoxing,us>
2014-06-29 01:49:07 +0800

    Add Chinese-Simplified Translations

---
+++ htdocs/application/language/chinese-simplified/form_validation_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['required']			= "%s 是必须的。";
+$lang['not_allowed']		= "你不被允许发布便签。";
+$lang['captcha']	    	= "验证码错误";
+$lang['blocked_words']    	= "你的便签包括了不被允许的字符。";
+$lang['robot']             	= "机器人走卡！";
+$lang['empty']				= "%s 不能为空";
+$lang['valid_lang']			= "请选择语言";
+$lang['isset']				= "%s 必须赋值。";
+$lang['valid_email']		= "%s 必须为一个有效的邮件地址。";
+$lang['valid_emails']		= "%s 只能包含所有有效的邮件地址。";
+$lang['valid_url']			= "%s 必须为一个有效的链接。";
+$lang['valid_ip']			= "%s 必须为一个有效的IP。";
+$lang['min_length']			= "%s 至少要有%s 个字符";
+$lang['max_length']			= "%s 最多只能有%s 个字符";
+$lang['exact_length']		= "%s 必须为%s 个字符";
+$lang['alpha']				= "%s 只能包含字母。";
+$lang['alpha_numeric']		= "%s 只能包含字母数字字符.";
+$lang['alpha_dash']			= "%s 只能包含字母数字字符，下划线和破折号。";
+$lang['numeric']			= "%s 只能包含数字。";
+$lang['is_numeric']			= "%s 只能包含数字字。";
+$lang['integer']			= "%s 必须包含一个整数";
+$lang['regex_match']		= "%s 格式不正确。";
+$lang['matches']			= "%s 不与 %s 相相匹配。";
+$lang['is_unique'] 			= "%s 必须为一个独一无二的数字。";
+$lang['is_natural']			= "%s 必须包含一个正数。";
+$lang['is_natural_no_zero']	= "%s 必须包含一个正数。";
+$lang['decimal']			= "%s 必须为一个十进制数。";
+$lang['less_than']			= "%s 必须包含一个小于%s的数字。";
+$lang['greater_than']		= "%s 必须包含一个数字大于%s的。";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/chinese-simplified/form_validation_lang.php */
