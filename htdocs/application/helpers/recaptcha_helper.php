f4fb3e8d52c27d85b12e00f197ccce386c61936a
Sebastian Korotkiewicz <wiesia300@gmail.com>
2014-12-22 21:55:00 +0100

    new google captcha

--- htdocs/application/helpers/recaptcha_helper.php
+++ htdocs/application/helpers/recaptcha_helper.php
@@ -119,13 +119,17 @@ function recaptcha_get_html ($pubkey, $error = null, $use_ssl = false)
         if ($error) {
            $errorpart = "&amp;error=" . $error;
         }
-        return '<script type="text/javascript" src="'. $server . '/challenge?k=' . $pubkey . $errorpart . '"></script>
+	return '<script src=\'https://www.google.com/recaptcha/api.js\'></script>
+		<div class="g-recaptcha" data-sitekey="' . $pubkey . '"></div>';
+
+
+        /*return '<script type="text/javascript" src="'. $server . '/challenge?k=' . $pubkey . $errorpart . '"></script>
 
 	<noscript>
   		<iframe src="'. $server . '/noscript?k=' . $pubkey . $errorpart . '" height="300" width="500" frameborder="0"></iframe><br/>
   		<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
   		<input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
-	</noscript>';
+	</noscript>';*/
 }
 
 
