4866c5b8de6db4a1453e3eee415ef53f1333e7f5
Ridho Muhammad <rydhoms@hotmail.co.id>
2016-03-18 08:52:00 +0700

    Improved Bahasa Indonesa language

--- htdocs/application/language/indonesia/form_validation_lang.php
+++ htdocs/application/language/indonesia/form_validation_lang.php
@@ -1,34 +1,34 @@
 <?php
 
-$lang['required']			= "Bidang %s butuh untuk diisi.";
+$lang['required']			= "Naskah butuh untuk diisi.";
 $lang['not_allowed']		= "Anda tidak diizinkan untuk membuat naskah.";
 $lang['captcha']	    	= "Captcha yang dimasukan salah.";
 $lang['blocked_words']    	= "Naskah Anda berisikan kata yang dilarang.";
 $lang['robot']             	= "Makan dulu sana (pergi), robot!";
-$lang['empty']				= "Bidang %s tidak boleh kosong.";
+$lang['empty']				= "%s tidak boleh kosong.";
 $lang['valid_lang']			= "Silahkan pilih bahasa yang diinginkan.";
-$lang['isset']				= "Bidang %s harus memiliki nilai.";
-$lang['valid_email']		= "Bidang %s harus berisi alamat email yang valid.";
-$lang['valid_emails']		= "Bidang %s harus berisi semua alamat email yang valid.";
-$lang['valid_url']			= "Bidang %s harus berisi URL yang valid.";
-$lang['valid_ip']			= "Bidang %s harus berisi IP yang valid.";
-$lang['min_length']			= "Bidang %s harus memiliki minimal %s karakter.";
-$lang['max_length']			= "Bidang %s tidak dapat melebihi %s karakter.";
-$lang['exact_length']		= "Bidang %s harus tepat %s panjang karakter.";
-$lang['alpha']				= "Bidang %s hanya dapat berisi karakter abjad.";
-$lang['alpha_numeric']		= "Bidang %s hanya dapat berisi karakter alfanumerik.";
-$lang['alpha_dash']			= "Bidang %s hanya dapat berisi karakter alfanumerik, garis bawah, dan garis.";
-$lang['numeric']			= "Bidang %s harus berisi hanya angka.";
-$lang['is_numeric']			= "Bidang %s harus berisi karakter numerik.";
-$lang['integer']			= "Bidang %s harus berisi sebuah integer.";
-$lang['regex_match']		= "Bidang %s tidak dalam format yang benar.";
-$lang['matches']			= "Bidang %s tidak sama dengan Bidang %s.";
-$lang['is_unique'] 			= "Bidang %s harus berisi nilai unik.";
-$lang['is_natural']			= "Bidang %s harus berisi angka positif.";
-$lang['is_natural_no_zero']	= "Bidang %s harus berisi angka yang lebih besar dari nol.";
-$lang['decimal']			= "Bidang %s harus berisi angka desimal.";
-$lang['less_than']			= "Bidang %s harus berisi angka kurang dari %s.";
-$lang['greater_than']		= "Bidang %s harus berisi angka yang lebih besar dari %s.";
+$lang['isset']				= "%s harus memiliki nilai.";
+$lang['valid_email']		= "%s harus berisi alamat email yang valid.";
+$lang['valid_emails']		= "%s harus berisi semua alamat email yang valid.";
+$lang['valid_url']			= "%s harus berisi URL yang valid.";
+$lang['valid_ip']			= "%s harus berisi IP yang valid.";
+$lang['min_length']			= "%s harus memiliki minimal %s karakter.";
+$lang['max_length']			= "%s tidak dapat melebihi %s karakter.";
+$lang['exact_length']		= "%s harus tepat %s panjang karakter.";
+$lang['alpha']				= "%s hanya dapat berisi karakter abjad.";
+$lang['alpha_numeric']		= "%s hanya dapat berisi karakter alfanumerik.";
+$lang['alpha_dash']			= "%s hanya dapat berisi karakter alfanumerik, garis bawah, dan garis.";
+$lang['numeric']			= "%s harus berisi hanya angka.";
+$lang['is_numeric']			= "%s harus berisi karakter numerik.";
+$lang['integer']			= "%s harus berisi sebuah integer.";
+$lang['regex_match']		= "%s tidak dalam format yang benar.";
+$lang['matches']			= "%s tidak sama dengan %s.";
+$lang['is_unique'] 			= "%s harus berisi nilai unik.";
+$lang['is_natural']			= "%s harus berisi angka positif.";
+$lang['is_natural_no_zero']	= "%s harus berisi angka yang lebih besar dari nol.";
+$lang['decimal']			= "%s harus berisi angka desimal.";
+$lang['less_than']			= "%s harus berisi angka kurang dari %s.";
+$lang['greater_than']		= "%s harus berisi angka yang lebih besar dari %s.";
 
 
 /* End of file form_validation_lang.php */
